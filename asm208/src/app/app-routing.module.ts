79feb2ae7ce712bd815950578fafffbfb2481af0
giangtvph14253 <giangtvph14253@fpt.edu.vn>
2022-10-18 02:19:39 +0700

    first commit

---
+++ asm208/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './service/admin/auth.guard';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { AdminComponent } from './pages/admin/admin.component';
+const routes: Routes = [
+  // path:"admin",canActivate:[AuthGuard] ,component: AdminLayoutComponent,
+  { path: "", component: HomeComponent },
+  {
+    path: "admin", component: AdminComponent, children: [
+      { path: "home", component: HomeComponent },
+      {
+        path: "list", component: ProductListComponent
+      },
+      { path: "add", component: ProductAddComponent },
+    ]
+  },
+  { path: "list/edit/:id", component: ProductAddComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ asm208/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import data from './data';
+import { IProduct } from './models/product';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+}
